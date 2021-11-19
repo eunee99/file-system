c3bf799a9cc5767a39a296816cc77d017f26ddd2
euning <52948715+eunee99@users.noreply.github.com>
2021-11-19 15:26:48 +0900

    Add files via upload

---
+++ common.h
@@ -0,0 +1,155 @@
+//here will be useful common functions
+/*
+유저 인증 및 macro 상수를 정의하는 파일 임.
+유저의 id, password를 저장하고 있으며,
+유저를 구분하기 위한 token 값을 상수로 define 하여 사용함.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define INIT_MSG "===================\n Hi this is p2p test plz login\n ===============\n"
+#define ID_REQ "input id : "
+#define PW_REQ "input password : "
+#define USER1_ID "user"
+#define USER1_PW "passwd1"
+#define USER2_ID "user2"
+#define USER2_PW "passwd2"
+
+#define USER1_LOGIN 1
+#define USER2_LOGIN 2
+#define LOGIN_FAIL 0
+#define BUFSIZE 512
+#define USER1_FTP_PORT 4141 //FTP를 위한 포트 번호
+#define USER2_FTP_PORT 4142 //FTP를 위한 포트 번호
+
+
+// 유저의 인증로직을 위한 함수.
+unsigned int authenticate(int fd, char *id, char *pw)
+{
+    send(fd, ID_REQ, strlen(ID_REQ) + 1, 0); //strlen((char *)some_string +1) 인 이유는 Null 포함
+    read(fd, id, sizeof(id));
+    send(fd, PW_REQ, strlen(PW_REQ) + 1, 0);
+    read(fd, pw, sizeof(pw));
+
+    printf("===========================\n");
+    printf("User Information\n");
+    printf("id : %s  pw : %s \n", id, pw);
+    printf("===========================\n");
+
+    if (strcmp(id, USER1_ID) == 0)
+    {
+        if (strcmp(pw, USER1_PW) == 0)
+        {
+            printf("%s Login success \n", id);
+            return USER1_LOGIN;
+        }
+        else
+        {
+            return LOGIN_FAIL;
+        }
+    }
+    else if (strcmp(id, USER2_ID) == 0)
+    {
+        if (strcmp(pw, USER2_PW) == 0)
+        {
+            printf("%s Login success \n", id);
+            return USER2_LOGIN;
+        }
+        else
+        {
+            return LOGIN_FAIL;
+        }
+    }
+    else
+        return LOGIN_FAIL;
+}
+
+
+void send_file(FILE *fp, int sockfd)
+{
+    int n;
+    char data[BUFSIZE] = {0};
+    int cnt = 0;
+    while (fgets(data, BUFSIZE, fp) != NULL)
+    {
+        cnt++;
+        if (send(sockfd, data, BUFSIZE, 0) == -1)
+        {
+            perror("[-]Error in sending file.");
+            exit(1);
+        }
+        bzero(data, BUFSIZE);
+    }
+    cnt++;
+    bzero(data, BUFSIZE);
+    strncpy(data, "DATSD\0", 6); //DATSD는 전송 완료되었음을 명시하기 위해서 사용
+    //보안에 취약점이 있지만 그냥 쓰자.
+    send(sockfd, data, BUFSIZE, 0);
+    return;
+}
+
+
+void write_file_to_fd(int sockfd, int fd)
+{   sleep(2);
+    int n;
+    FILE *fp;
+    char buffer[BUFSIZE];
+    int cnt = 0;
+    int cmp_num;
+
+    fp = fdopen(fd, "w");
+    if (fp == NULL)
+    {
+        perror("open");
+    }
+    while (1)
+    {
+        cnt++;
+        n = recv(sockfd, buffer, BUFSIZE, 0); //마지막에 EOF를 던져주지 않으면 블로킹 당함.
+        if (n <= 0)
+        { //EOF -> 소켓이 끊어지면 전달된다 (n = 0)
+            // n < 0 -> recv 에러.
+            break;
+            return;
+        }
+        cmp_num = strncmp(buffer, "DATSD", 5);
+        if (cmp_num == 0)
+        {
+            break;
+            return;
+        }
+        fprintf(fp, "%s", buffer);
+        bzero(buffer, BUFSIZE);
+    }
+    fclose(fp);
+    return;
+}
+
+void print_recv_file(int sockfd)
+{
+    int n;
+    char buffer[BUFSIZE];
+    while (1)
+    {
+        n = recv(sockfd, buffer, BUFSIZE, 0);
+        if (n <= 0)
+        {
+            break;
+            return;
+        }
+        if ((strcmp(buffer, "DATSD")) == 0)
+        {
+            printf("file recv done!\n");
+            break;
+            return;
+        }
+        printf("%s", buffer);
+        bzero(buffer, BUFSIZE);
+    }
+    return;
+}
